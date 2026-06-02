1a9a9ab82098591a293c44dda68157ec92cf11e6
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-06-02 12:10:29 +0500

    Day 2 Of Learning  Python (Variables, strings , floats ).py
    I learn variable, string, and floats uses that help me so much in my python carier

--- #2.py
+++ #2.py
@@ -1,4 +1,4 @@
-Name = input("What Is Your Name:         ")
+ Name = input("What Is Your Name:         ")
 Age = input("How Old Are You :      ")
 Hight = input("What Is Your Current Hight:      ")
 print(f"{Name} Listen Carefully! Your hight is {Hight} tgat doesnt meet our criteria and your Age {Age Is Too Much For That job Sorry! We Can't Hire You")
