593c16c83cb15aa58cda2cb32887a695e5bc04af
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-06-04 06:46:41 +0500

    Age Priductor Calculator.py
    I Build An Age Pricdictor Calculator With The Help Of Allah.....And I Learn The Logic Of The Core of this System

---
+++ #4.py
@@ -0,0 +1,6 @@
+print("-----------------Welcome To Age Pridiction-------------")
+Current_Year = int(input("Type the Current Year Like 2026,2027 etc:        "))
+Age = int(input("How Old Are You???:                              "))
+User_Age =  Age + 10
+Future_Year = Current_Year + 10
+print(f"You Will Be {User_Age} Years Old In {Future_Year}......InshaAllah.....")
