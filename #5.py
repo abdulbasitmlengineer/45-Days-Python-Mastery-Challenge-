315d61ce0eb7d47b0250c1850bf145e13b6a2e68
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-06-05 08:47:09 +0500

    Strung Formating.py
    Today I Learn About String Formating And Use f sting To make my program more efficiently

---
+++ #5.py
@@ -0,0 +1,7 @@
+name = input("What Is Your Name??       ")
+age = input("how old are you ???       ")
+game = input("Favorite Game???          ")
+print(f"Hello {name}")
+
+print(f"You'r {age} Years Old.")         
+print(f"Your Favorite Game Is {game} ")
