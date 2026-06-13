dca962a032b0136a5eee95271af6b323f5551d0b
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-06-13 16:17:10 +0500

    Day13:Even number 1to 100
    Fix the logic to correctly generate even numbers from 0 to 100.

---
+++ 1 TO 100 Even Numbers.py
@@ -0,0 +1,8 @@
+print("========================== WELCOME TO EVEN NUMBBER GENERATOR Program ========================")
+for i in range(0,101):
+		while i != 100:
+			i = i+2
+			if i <= 100:
+				print(i)
+		exit()
+		
