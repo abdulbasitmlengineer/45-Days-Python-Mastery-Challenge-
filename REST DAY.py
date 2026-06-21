727a94eae568bde98dd6c6702232e3ec98dda698
ABDUL BASIT <abdulbasitmlengineer@gmail.com>
2026-06-21 10:43:28 +0500

    DAY21:REST DAY

---
+++ REST DAY.py
@@ -0,0 +1,7 @@
+print("----------REST DAY--------------")
+name = input("WHAT IS YOUR NAME:")
+day = input("TYPE DAY NAME:")
+if day == "sunday":
+  print("CONGRATULATIONS TODAY IS YOUR REST DAY")
+print("DO YOUR PYTHON CODE")
+
