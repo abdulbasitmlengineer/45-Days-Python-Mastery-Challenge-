36a3ebf64da3f11144621fb9860c7f529f231143
ABDUL BASIT <abdulbasitmlengineer@gmail.com>
2026-06-25 19:42:08 +0500

    TIRED DAY REST

---
+++ Tired Day.py
@@ -0,0 +1,6 @@
+print("------------- TIRED DAY ---------------")
+user = input("Are you feeling well(y/n):")
+while user.lower() == "y":
+  print("TAKE SOME REST🥱.Eat some snacks🦒....\nREMEMBER🚨, DO NOT STUDY UNTIL YOU FEEL BETTER ")
+  user = input("\nAre you feeling well(y/n):")
+print("GO AND STUDY. HURRYUP😅")
